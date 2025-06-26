1a69adfa3297d91bd7f4420b01449fe040f2d459
Martin Anev <martinanev333@gmail.com>
2025-06-27 00:02:14 +0300

    Remove unnecessary folder

--- UrlShortener/Models/UrlViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UrlShortener.Models
-{
-    public class UrlViewModel
-    {
-        public string Url { get; set; } = string.Empty;
-        public string ShortenedUrl { get; set; } = string.Empty;
-        public string StatsUrl { get; set; } = string.Empty;
-    }
-}
